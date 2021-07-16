c44070e63c316fbb25462cd5c93a85e5c525dbda
Sarah <82398047+Helcony@users.noreply.github.com>
2021-07-16 00:31:08 -0300

    Add files via upload

---
+++ mochaexpress/main.js
@@ -0,0 +1,79 @@
+/* TOGGLE SHOW (MENU) */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* CLOSE MENU WHEN AN ITEM'S CLICKED */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* HEADER SCROLL */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // greater or equal than the header's height
+    header.classList.add('scroll')
+  } else {
+    // lesser then the header's height
+    header.classList.remove('scroll')
+  }
+})
+
+/* TESTIMONIALS SWIPER */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  loop: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* SCROLLREVEAL */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #products header, #products .card,
+  #about .image, #about .text,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .icon, #contact .map,
+  footer .logo, footer p, footer .social`,
+  { interval: 100 }
+)
+
+/* BACK TO TOP */
+const backToTopButton = document.querySelector('.back-to-top')
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 700) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
